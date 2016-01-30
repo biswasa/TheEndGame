28708328a600b838462ff659a55475b012f8dc03
Abhishek Biswas <b100.abhishek@gmail.com>
2016-01-29 23:28:43 -0800

    Scene walls/lighting

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,31 +1,35 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerMovement : MonoBehaviour {
+public class PlayerMovement : MonoBehaviour
+{
 
 	private float movementSpeed = 10.0f;
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		movePlayer();
+	void Update ()
+	{
+		movePlayer ();
 	}
 	
 	// Governs degrees of freedom of player movement
-	void movePlayer() {
-		Vector3 moveVector = new Vector3(0,0,0);
+	void movePlayer ()
+	{
+		Vector3 moveVector = new Vector3 (0, 0, 0);
 		
-		if(Input.GetKey(KeyCode.LeftArrow))	{
+		if (Input.GetKey (KeyCode.LeftArrow)) {
 			moveVector.x -= movementSpeed * Time.deltaTime;
-		} else if(Input.GetKey(KeyCode.RightArrow)) {
+		} else if (Input.GetKey (KeyCode.RightArrow)) {
 			moveVector.x += movementSpeed * Time.deltaTime;
-		} else if(Input.GetKey(KeyCode.DownArrow)) {
+		} else if (Input.GetKey (KeyCode.DownArrow)) {	
 			moveVector.z -= movementSpeed * Time.deltaTime;
-		} else if(Input.GetKey(KeyCode.UpArrow)) {
+		} else if (Input.GetKey (KeyCode.UpArrow)) {
 			moveVector.z += movementSpeed * Time.deltaTime;
 		}
 		
