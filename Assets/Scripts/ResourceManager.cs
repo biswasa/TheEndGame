203fba888a4d71a211196dc94240f3f868ce5b78
Abhishek Biswas <b100.abhishek@gmail.com>
2016-01-30 01:48:28 -0800

    Randomization of resource spawner motion

--- Assets/Scripts/ResourceManager.cs
+++ Assets/Scripts/ResourceManager.cs
@@ -3,13 +3,51 @@ using System.Collections;
 
 public class ResourceManager : MonoBehaviour {
 
+	public Transform resourcePrefab;
+	
+	private const float MIN_RESPAWN_TIME = 10.0f;
+	private float resourceSpawnerSpeed = 15.0f;
+	private int maxResources = 5;
+	public int resources;
+	public float lastRespawn;
+
 	// Use this for initialization
 	void Start () {
-	
+		resources = 0;	
+		lastRespawn = Time.time;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		moveResourceSpawner();
+		if ((resources < maxResources) && ((Time.time - lastRespawn) > (1 + Random.value) * MIN_RESPAWN_TIME)) {
+			respawn();
+		}
+	}
 	
+	// Creates a new resource at the spawner location
+	void respawn() {
+		Object spawnedResource = Instantiate(resourcePrefab, transform.position, Quaternion.identity);
+		
+		// Sanity checking
+		if(spawnedResource != null) {
+			resources++;
+			lastRespawn = Time.time;
+		}
+	}
+		
+	// Randomly moves resource spawner within player-accessible area
+	void moveResourceSpawner() {
+		float speed = resourceSpawnerSpeed * Time.deltaTime;
+		float randOrientation = Random.value;
+		if (randOrientation <= 0.25f) {	
+			transform.position += new Vector3((1 + Random.value) * speed, 0, (1 + Random.value) * speed);
+		} else if (randOrientation <= 0.5f) {
+			transform.position += new Vector3(-(1 + Random.value) * speed, 0, (1 + Random.value) * speed);	
+		} else if (randOrientation <= 0.75f) {
+			transform.position += new Vector3((1 + Random.value) * speed, 0, -(1 + Random.value) * speed);
+		} else {
+			transform.position += new Vector3(-(1 + Random.value) * speed, 0, -(1 + Random.value) * speed);
+		}
 	}
 }
