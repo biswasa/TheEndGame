b17ed4f4bda1fac10f47801d7131a307a14d1288
Abhishek Biswas <b100.abhishek@gmail.com>
2016-01-29 23:53:33 -0800

    Consolidated scripts

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -3,13 +3,38 @@ using System.Collections;
 
 public class PlayerManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+	private const float MAX_HEALTH = 50.0f;
+	private const int MAX_RESOURCES = 10;
+	private float movementSpeed = 10.0f;
+	private float currHealth;
+	private int currResources;
 	
+	// Use this for initialization
+	void Start() {
+		currHealth = MAX_HEALTH;
+		currResources = 0;
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update() {
+		movePlayer();	
+	}
 	
+	// Governs degrees of freedom of player movement
+	void movePlayer()
+	{
+		Vector3 moveVector = new Vector3 (0, 0, 0);
+		
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			moveVector.x -= movementSpeed * Time.deltaTime;
+		} else if (Input.GetKey (KeyCode.RightArrow)) {
+			moveVector.x += movementSpeed * Time.deltaTime;
+		} else if (Input.GetKey (KeyCode.DownArrow)) {	
+			moveVector.z -= movementSpeed * Time.deltaTime;
+		} else if (Input.GetKey (KeyCode.UpArrow)) {
+			moveVector.z += movementSpeed * Time.deltaTime;
+		}
+		
+		transform.position += moveVector;
 	}
 }
