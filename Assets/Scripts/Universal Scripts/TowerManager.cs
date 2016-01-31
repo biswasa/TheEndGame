e35c0a0c106be02e58a4212a29cd283d3db15c91
Abhishek Biswas <b100.abhishek@gmail.com>
2016-01-30 20:17:21 -0800

    Tower triggers added

---
+++ Assets/Scripts/Universal Scripts/TowerManager.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TowerManager : MonoBehaviour {
+
+	private const float MAX_HEALTH = 100.0f;
+	private enum RESOURCE_TIERS {ZERO=0, ALPHA=10, BETA=25, GAMMA=50}; // Number of resources required to level up
+	private float depositDelay = 2.0f; // Time taken to deposit 1 unit resource
+	private float depositStartTime;
+	private float currHealth;
+	private int currResources;
+	private bool depositAllowed;
+	
+	// Use this for initialization
+	void Start () {
+		depositStartTime = 0.0f	;
+		currHealth = 0.0f;
+		currResources = 0;
+		depositAllowed = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	public bool canDeposit() {
+		return depositAllowed;	
+	}	
+	
+	// Starts counting time elapsed for single resource deposit
+	public void startDeposit() {
+		depositStartTime = Time.time;
+	}
+	
+	// Deposits items if enough time has elasped; true -> deposited
+	public bool deposit() {
+		if ((Time.time - depositStartTime) > depositDelay) {
+			depositStartTime = Time.time;
+			++currResources;	
+			return true;	
+		} else {
+			return false;
+		}
+	}
+	
+	public void playerInRange(Collider temp) {
+		// Local variable declaration
+		string tagCheck = temp.transform.parent.gameObject.tag;
+		Vector3 self = transform.position, other = temp.transform.parent.transform.position;
+		GameObject selfTrigger = GameObject.FindWithTag("TowerTrigger");
+		float radius = 0.0f;
+		
+		// Check origin of trigger
+		if (tagCheck.Equals("Player")) {
+			SphereCollider otherTrigger = (SphereCollider)temp;			
+			radius = selfTrigger.GetComponent<SphereCollider>().radius + otherTrigger.radius;
+		} else {
+			depositAllowed = false;
+			return;
+		}
+		
+		// Compare actual distance to threshold	
+		if (Vector3.Distance(self, other) >= radius) {
+			Debug.Log("Player Out");
+			depositAllowed = false;
+		} else {
+			Debug.Log("Player In");
+			depositAllowed = true;
+		}
+	}
+}
